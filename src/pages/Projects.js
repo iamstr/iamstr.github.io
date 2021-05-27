8e89b1b1506ee73aa8034a1538ee793e59169294
iamstr <asatar1994@gmail.com>
2021-05-27 05:23:46 +0300

    finished skills page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,13 +1,114 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import IMG1 from "../assets/img/giphy.png";
 import Navbar from "../components/Navbar";
-
 export default function Projects() {
   return (
     <>
       <Navbar />
 
-      <main>
+      <main className="main">
         <h1>Projects</h1>
+        <article className="card flex d-column">
+          <img src={IMG1} alt="" className="card-img" />
+          <div className="card-info">
+            <h4 className="card-header">Project 1</h4>
+            <p className="card-para">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem
+              molestias sapiente aperiam quibusdam nobis. Quaerat voluptatem
+              eligendi dolorum sed asperiores...
+            </p>
+          </div>
+
+          <div className="card-btns flex d-row justify-content-between">
+            <Link to="/projects">
+              <button className="btn-outline">More info</button>
+            </Link>
+            <Link to="/projects">
+              <button className="btn-outline-white">Check repo</button>
+            </Link>
+          </div>
+        </article>
+        <article className="card flex d-column">
+          <img src={IMG1} alt="" className="card-img" />
+          <div className="card-info">
+            <h4 className="card-header">Project 1</h4>
+            <p className="card-para">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem
+              molestias sapiente aperiam quibusdam nobis. Quaerat voluptatem
+              eligendi dolorum sed asperiores...
+            </p>
+          </div>
+
+          <div className="card-btns flex d-row justify-content-between">
+            <Link to="/projects">
+              <button className="btn-outline">More info</button>
+            </Link>
+            <Link to="/projects">
+              <button className="btn-outline-white">Check repo</button>
+            </Link>
+          </div>
+        </article>
+        <article className="card flex d-column">
+          <img src={IMG1} alt="" className="card-img" />
+          <div className="card-info">
+            <h4 className="card-header">Project 1</h4>
+            <p className="card-para">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem
+              molestias sapiente aperiam quibusdam nobis. Quaerat voluptatem
+              eligendi dolorum sed asperiores...
+            </p>
+          </div>
+
+          <div className="card-btns flex d-row justify-content-between">
+            <Link to="/projects">
+              <button className="btn-outline">More info</button>
+            </Link>
+            <Link to="/projects">
+              <button className="btn-outline-white">Check repo</button>
+            </Link>
+          </div>
+        </article>
+        <article className="card flex d-column">
+          <img src={IMG1} alt="" className="card-img" />
+          <div className="card-info">
+            <h4 className="card-header">Project 1</h4>
+            <p className="card-para">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem
+              molestias sapiente aperiam quibusdam nobis. Quaerat voluptatem
+              eligendi dolorum sed asperiores...
+            </p>
+          </div>
+
+          <div className="card-btns flex d-row justify-content-between">
+            <Link to="/projects">
+              <button className="btn-outline">More info</button>
+            </Link>
+            <Link to="/projects">
+              <button className="btn-outline-white">Check repo</button>
+            </Link>
+          </div>
+        </article>
+        <article className="card flex d-column">
+          <img src={IMG1} alt="" className="card-img" />
+          <div className="card-info">
+            <h4 className="card-header">Project 1</h4>
+            <p className="card-para">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem
+              molestias sapiente aperiam quibusdam nobis. Quaerat voluptatem
+              eligendi dolorum sed asperiores...
+            </p>
+          </div>
+
+          <div className="card-btns flex d-row justify-content-between">
+            <Link to="/projects">
+              <button className="btn-outline">More info</button>
+            </Link>
+            <Link to="/projects">
+              <button className="btn-outline-white">Check repo</button>
+            </Link>
+          </div>
+        </article>
       </main>
     </>
   );
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import Menu from "../assets/img/hamburger-menu.svg";
+import BTS from "../assets/img/Icon awesome-bootstrap.svg";
+import Gulp from "../assets/img/Icon awesome-gulp.svg";
+import JS from "../assets/img/Icon awesome-js-square.svg";
+import RCT from "../assets/img/Icon awesome-react.svg";
+import SASS from "../assets/img/Icon awesome-sass.svg";
+import LINT from "../assets/img/Icon simple-eslint.svg";
+import GATSBY from "../assets/img/Icon simple-gatsby.svg";
+import NEXT from "../assets/img/Icon simple-next-dot-js.svg";
+import WB from "../assets/img/Icon simple-webpack.svg";
+import CSS3 from "../assets/img/Path 4.svg";
 import Navbar from "../components/Navbar";
+
 export default function Skills() {
+  const [open, setOpen] = useState(false);
   return (
     <>
+      <img src={Menu} alt="" class="hamburger" onClick={() => setOpen(true)} />
       <Navbar />
 
       <main>
         <article></article>
         <h1>Skills</h1>
+        <article className="flex justify-content-between flex-row skill-container">
+          <img src={JS} alt="" />
+          <img src={SASS} alt="" />
+          <img src={RCT} alt="" />
+          <img src={CSS3} alt="" />
+        </article>
         <h3>Build Tools</h3>
-        <h3>Library</h3>
+        <article className="flex justify-content-between flex-row skill-container">
+          <img src={Gulp} alt="" />
+          <img src={WB} alt="" />
+          <img src={LINT} alt="" />
+        </article>
+
         <h3>Frameworks</h3>
-        <article></article>
-        <article></article>
-        <article></article>
+        <article className="flex justify-content-between flex-row skill-container">
+          <img src={GATSBY} alt="" />
+          <img src={NEXT} alt="" />
+        </article>
+        <h3>UI</h3>
+        <article className="flex justify-content-between flex-row skill-container">
+          <img src={BTS} alt="" />
+        </article>
       </main>
     </>
   );
